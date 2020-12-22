1d5d03a1f8007b00dec21711ea675f42e09f9b0b
Isabellelimaa <isabellelima.fis@gmail.com>
2020-12-22 17:51:30 -0300

    Alteracoes Redux

--- src/components/cadastro/Cadastro.js
+++ src/components/cadastro/Cadastro.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   Container,
   FormGroup,
@@ -8,30 +9,79 @@ import {
 } from "@material-ui/core";
 import "./styles.css";
 
-const Cadastro = (props) => (
-  <Container className="cadastroContainer">
-    <FormGroup className="cadastroItens">
-      <InputLabel>Nome</InputLabel>
-      <Input type="text" required></Input>
-    </FormGroup>
-    <FormGroup className="cadastroItens">
-      <InputLabel>E-mail</InputLabel>
-      <Input type="email" required></Input>
-    </FormGroup>
-    <FormGroup className="cadastroItens">
-      <InputLabel>Confirme o e-mail</InputLabel>
-      <Input type="email" required></Input>
-    </FormGroup>
-    <FormGroup className="cadastroItens">
-      <InputLabel>Senha</InputLabel>
-      <Input type="password" required></Input>
-    </FormGroup>
-    <FormGroup className="cadastroItens">
-      <InputLabel>Confirme a senha</InputLabel>
-      <Input type="password" required></Input>
-    </FormGroup>
-    <Button>Enviar</Button>
-  </Container>
-);
+import { setListaCadastro } from "../../Redux/AppReducers/AppReducers";
+
+const Cadastro = (props) => {
+  const [cadastro, setCadastro] = useState({});
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    dispatch(setListaCadastro(cadastro.nome));
+  };
+
+  const handleChange = (e) => {
+    setCadastro({
+      ...cadastro,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <Container className="cadastroContainer">
+      <FormGroup className="cadastroItens">
+        <InputLabel>Nome</InputLabel>
+        <Input
+          type="text"
+          name="nome"
+          value={cadastro.nome}
+          onChange={handleChange}
+          required
+        ></Input>
+      </FormGroup>
+      <FormGroup className="cadastroItens">
+        <InputLabel>E-mail</InputLabel>
+        <Input
+          type="email"
+          name="email"
+          value={cadastro.email}
+          onChange={handleChange}
+          required
+        ></Input>
+      </FormGroup>
+      <FormGroup className="cadastroItens">
+        <InputLabel>Confirme o e-mail</InputLabel>
+        <Input
+          type="email"
+          name="cEmail"
+          value={cadastro.cEmail}
+          onChange={handleChange}
+          required
+        ></Input>
+      </FormGroup>
+      <FormGroup className="cadastroItens">
+        <InputLabel>Senha</InputLabel>
+        <Input
+          type="password"
+          name="senha"
+          value={cadastro.senha}
+          onChange={handleChange}
+          required
+        ></Input>
+      </FormGroup>
+      <FormGroup className="cadastroItens">
+        <InputLabel>Confirme a senha</InputLabel>
+        <Input
+          type="password"
+          name="cSenha"
+          value={cadastro.cSenha}
+          onChange={handleChange}
+          required
+        ></Input>
+      </FormGroup>
+      <Button onClick={handleSubmit}>Enviar</Button>
+    </Container>
+  );
+};
 
 export default Cadastro;
--- src/components/lista/Lista.js
+++ src/components/lista/Lista.js
@@ -1,26 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { List, ListItem, ListItemText } from "@material-ui/core";
 import "./styles.css";
 
 const Lista = (props) => {
-  function generate(element) {
-    // return props.itens.map((value) =>
-    //   React.cloneElement(element, {
-    //     title: value,
-    //   })
-    // );
+  const cadastroDataStore = useSelector(
+    (store) => store.AppReducer.listaCadastro
+  );
 
-    return [0, 1, 2].map((value) => React.cloneElement(element, {}));
-  }
+  const generate = () => {
+    return cadastroDataStore.map((value, index) => {
+      return (
+        <ListItem key={index}>
+          <ListItemText primary={value} />
+        </ListItem>
+      );
+    });
+  };
+
   return (
     <div className="listaDiv">
-      <List>
-        {generate(
-          <ListItem>
-            <ListItemText primary="Single-line item" />
-          </ListItem>
-        )}
-      </List>
+      <List>{generate()}</List>
     </div>
   );
 };
